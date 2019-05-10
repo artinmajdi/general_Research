6a29538bdf8ab783607fa1f462de6098b2140e58
artinmajdi <msm2024@gmail.com>
2019-05-10 14:35:39 -0700

    vbc

--- main_uncrop.py
+++ main_uncrop.py
@@ -11,19 +11,22 @@ import nibabel as nib
 # params.directories.Train.address
 # subF = smallFuncs.listSubFolders(params.directories.Train.address)
 
-dirr = '/array/ssd/msmajdi/experiments/keras/exp4/test/Main/vimp2_case3/' 
+subject = 'vimp2_Case3_WMn'
+
+dirr = '/array/ssd/msmajdi/experiments/keras/exp4/test/Main/' 
 dir_ref    = '/array/ssd/msmajdi/experiments/keras/exp3/train/Main/vimp2_819_05172013_DS/'
 
+subject_addr_out = smallFuncs.mkDir(dirr + subject + '_b')
 
 ref = nib.load(dir_ref + 'WMnMPRAGE_bias_corr.nii.gz')
-im = nib.load(dirr + 'LIFUP003_MPRAGE_WMn.nii.gz')
+im = nib.load(dirr + subject + '/orig.nii.gz')
 
 im2 = niImage.resample_img(img=im , target_affine=ref.affine  , target_shape=ref.get_data().shape , interpolation='continuous')
-nib.save(im2, dirr + 'WMnMPRAGE_bias_corr.nii.gz')
+nib.save(im2, subject_addr_out + '/WMnMPRAGE_bias_corr.nii.gz')
 
 
-dir_in = dirr + 'left/'
-dir_out = smallFuncs.mkDir(dirr + 'Label/')
+dir_in  = dirr + subject + '/left/'
+dir_out = smallFuncs.mkDir(subject_addr_out + '/Label/')
 for label in smallFuncs.Nuclei_Class(method='Cascade').All_Nuclei().Names:
 
     print(label)
